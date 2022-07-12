387558ad776eb114500e6c33ff8652d4faaa0616
medcl <m@medcl.net>
2022-07-12 17:56:05 +0800

    update agent

--- config/generated.go
+++ config/generated.go
@@ -1,10 +1,10 @@
 package config
 
-const LastCommitLog = "1459ce1d1282d0a896726639c8a85d2396b0d870"
-const BuildDate = "2022-03-23T10:40:20Z"
+const LastCommitLog = "N/A"
+const BuildDate = "N/A"
 
-const EOLDate  = "2023-12-31T10:10:10Z"
+const EOLDate = "N/A"
 
-const Version  = "1.0.0_SNAPSHOT"
+const Version = "0.0.1-SNAPSHOT"
 
-const BuildNumber  = "001"
+const BuildNumber = "001"
