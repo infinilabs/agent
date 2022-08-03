12e92f91ddf985eb295602d6efa2e8ea2c36bd1f
Cheng Kai <chengkai@infini.ltd>
2022-08-03 16:51:50 +0800

    agent register api ing

--- model/host.go
+++ model/host.go
@@ -5,14 +5,30 @@
 package model
 
 type Host struct {
-	IPs      []string  `json:"ip,omitempty"`
-	Clusters []Cluster `json:"clusters,omitempty"`
-	TLS      bool      `json:"tls"`
+	IPs       []string   `json:"ip,omitempty"`
+	TLS       bool       `json:"tls"`
+	AgentPort int        `json:"agent_port"`
+	AgentID   string     `json:"agent_id" yaml:"agent_id"`
+	Clusters  []*Cluster `json:"clusters" yaml:"clusters"`
 }
 
 type Cluster struct {
-	Name     string `json:"cluster_name,omitempty"`
-	ID       string `json:"cluster_id,omitempty"`
-	UserName string `json:"user_name,omitempty"`
-	Password string `json:"password,omitempty"`
+	Name     string  `json:"cluster.name,omitempty" yaml:"cluster.name"`
+	UUID     string  `json:"cluster.uuid,omitempty" yaml:"cluster.uuid"`
+	UserName string  `json:"username,omitempty" yaml:"username"`
+	Password string  `json:"password,omitempty" yaml:"password"`
+	Nodes    []*Node `json:"nodes" yaml:"nodes"`
+}
+
+type Node struct {
+	HttpPort   int    `json:"http.port,omitempty" yaml:"http.port,omitempty"`
+	ConfigPath string `json:"-" yaml:"-"`
+	Ports      []int  `json:"-" yaml:"-"` //之所以是数组，因为从进程信息中获取到端口会有多个(通常为2个)，需要二次验证。这个字段只做缓存
+}
+
+type ConsoleConfig struct {
+	Name string `json:"name" yaml:"name"`
+	Host string `json:"host" yaml:"host"`
+	Port int    `json:"port" yaml:"port"`
+	TLS  bool   `json:"tls" yaml:"tls"`
 }
