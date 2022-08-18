3af5dddd97aed10121ccf0d7a90e4a0f5ed3bf9f
Cheng Kai <chengkai@infini.ltd>
2022-08-18 17:39:19 +0800

    完成了数据采集,这个版本完全按照之前的采集逻辑，不再注册节点维度的metadata

--- plugin/manage/elastic-metadata/metadata.go
+++ plugin/manage/elastic-metadata/metadata.go
@@ -1,88 +1,71 @@
 package metadata
 
 import (
+	log "github.com/cihub/seelog"
 	"infini.sh/agent/model"
 	"infini.sh/framework/core/elastic"
 	"infini.sh/framework/modules/elastic/common"
-	"log"
+	"strings"
 )
 
 func Init(hostInfo *model.Host) {
-	RegisterMetadata(hostInfo)
+	registerMetadata(hostInfo)
 }
 
-func RegisterMetadata(hostInfo *model.Host) {
+func registerMetadata(hostInfo *model.Host) {
 	for i := 0; i < len(hostInfo.Clusters); i++ {
 		cluster := hostInfo.Clusters[i]
 		if cluster.Task != nil && cluster.Task.ClusterMetric == (model.ClusterMetricTask{}) && !cluster.Task.ClusterMetric.Owner {
 			continue //当前是console在采集，无需注册
 		}
-		for i := 0; i < len(cluster.Nodes); i++ {
-			node := cluster.Nodes[i]
-			clusterTaskOwner := false
-			if node.ID == cluster.Task.ClusterMetric.TaskNodeID {
-				clusterTaskOwner = true
-			}
-			err := InitOrUpdateElasticClient(node.ID, cluster.UserName, cluster.Password, cluster.Version, node.GetEndPoint(cluster.GetSchema()), clusterTaskOwner)
-			if err != nil {
-				log.Printf("RegisterMetadata Failed, %v\n", err)
-				continue
-			}
+		if err := initMetadata(cluster); err != nil {
+			log.Warnf("initMetadata err: %v", err)
 		}
 	}
-
 }
 
 func HostInfoChanged(newHostInfo *model.Host) {
-	log.Printf("metadata:  rigister/update\n")
+	log.Debugf("metadata.HostInfoChanged:  register/update\n")
 	if newHostInfo == nil {
 		return
 	}
-	RegisterMetadata(newHostInfo)
+	registerMetadata(newHostInfo)
 }
 
-func InitOrUpdateElasticClient(esNodeId string, userName string, password string, esVersion string, host string, clusterOwner bool) error {
-
-	elasticSearchConfig := &elastic.ElasticsearchConfig{BasicAuth: &struct {
+func initMetadata(cluster *model.Cluster) error {
+	escfg := &elastic.ElasticsearchConfig{BasicAuth: &struct {
 		Username string `json:"username,omitempty" config:"username" elastic_mapping:"username:{type:keyword}"`
 		Password string `json:"password,omitempty" config:"password" elastic_mapping:"password:{type:keyword}"`
 	}{
-		Username: userName,
-		Password: password,
+		Username: cluster.UserName,
+		Password: cluster.Password,
 	}}
-	elasticSearchConfig.ID = esNodeId
-	elasticSearchConfig.Version = esVersion
-	elasticSearchConfig.Name = esNodeId
-	elasticSearchConfig.Enabled = true
-	elasticSearchConfig.Monitored = true
-	elasticSearchConfig.Endpoint = host
-	elasticSearchConfig.MetadataConfigs = &elastic.MetadataConfig{
+	clusterEndpoint := GetClusterTaskEndPoint(cluster)
+	escfg.ID = cluster.ID
+	escfg.Version = cluster.Version
+	escfg.Name = cluster.UUID
+	escfg.Enabled = true
+	escfg.Monitored = true
+	escfg.Endpoint = clusterEndpoint
+	escfg.MetadataConfigs = &elastic.MetadataConfig{
 		HealthCheck:           elastic.TaskConfig{Enabled: true},
 		ClusterSettingsCheck:  elastic.TaskConfig{Enabled: false},
 		MetadataRefresh:       elastic.TaskConfig{Enabled: false},
 		NodeAvailabilityCheck: elastic.TaskConfig{Enabled: true},
 	}
-	client, err := common.InitClientWithConfig(*elasticSearchConfig)
+	client, err := common.InitClientWithConfig(*escfg)
 	if err != nil {
 		return err
 	}
-	elastic.RegisterInstance(*elasticSearchConfig, client)
-	elastic.GetOrInitHost(host, esNodeId)
-	metadata := elastic.GetOrInitMetadata(elasticSearchConfig)
-	nodesInfo := make(map[string]elastic.NodesInfo)
-	nodesInfo[esNodeId] = elastic.NodesInfo{
-		Http: struct {
-			BoundAddress            []string `json:"bound_address"`
-			PublishAddress          string   `json:"publish_address,omitempty"`
-			MaxContentLengthInBytes int64    `json:"max_content_length_in_bytes,omitempty"`
-		}{
-			PublishAddress: host,
-		},
-	}
-	metadata.Nodes = &nodesInfo
+	elastic.RegisterInstance(*escfg, client)
+	elastic.GetOrInitHost(removeUrlSchema(clusterEndpoint), cluster.ID)
+	metadata := elastic.GetOrInitMetadata(escfg)
+	metadata.IsAgentMode = true
+	metadata.Nodes = GetNodesInfo(cluster)
+
 	metadata.Config.MonitorConfigs = &elastic.MonitorConfig{
 		ClusterStats: elastic.TaskConfig{
-			Enabled:  false,
+			Enabled:  true,
 			Interval: "10s",
 		},
 		NodeStats: elastic.TaskConfig{
@@ -90,14 +73,56 @@ func InitOrUpdateElasticClient(esNodeId string, userName string, password string
 			Interval: "10s",
 		},
 		ClusterHealth: elastic.TaskConfig{
-			Enabled:  clusterOwner,
+			Enabled:  true,
 			Interval: "10s",
 		},
 		IndexStats: elastic.TaskConfig{
-			Enabled:  false,
+			Enabled:  true,
 			Interval: "10s",
 		},
 	}
-	elastic.SetMetadata(esNodeId, metadata)
+	elastic.SetMetadata(cluster.ID, metadata)
 	return nil
+}
+
+func GetNodesInfo(cluster *model.Cluster) *map[string]elastic.NodesInfo {
+
+	nodesInfo := make(map[string]elastic.NodesInfo)
+	for i := 0; i < len(cluster.Nodes); i++ {
+		node := cluster.Nodes[i]
+		nodesInfo[node.ID] = elastic.NodesInfo{
+			Http: struct {
+				BoundAddress            []string `json:"bound_address"`
+				PublishAddress          string   `json:"publish_address,omitempty"`
+				MaxContentLengthInBytes int64    `json:"max_content_length_in_bytes,omitempty"`
+			}{
+				BoundAddress:   []string{removeUrlSchema(node.GetEndPoint(cluster.GetSchema()))},
+				PublishAddress: removeUrlSchema(node.GetEndPoint(cluster.GetSchema())),
+			},
+		}
+	}
+	return &nodesInfo
+}
+
+// GetClusterTaskEndPoint
+// @Description: 要根据console指定的节点来拿endpoint，用来采集集群层面的指标。
+//
+func GetClusterTaskEndPoint(cluster *model.Cluster) string {
+	for i := 0; i < len(cluster.Nodes); i++ {
+		node := cluster.Nodes[i]
+		if node.ID == cluster.Task.ClusterMetric.TaskNodeID {
+			return node.GetEndPoint(cluster.GetSchema())
+		}
+	}
+	return ""
+}
+
+func removeUrlSchema(url string) string {
+	if url == "" {
+		return ""
+	}
+	if strings.Contains(url, "https://") {
+		return strings.ReplaceAll(url, "https://", "")
+	}
+	return strings.ReplaceAll(url, "http://", "")
 }
