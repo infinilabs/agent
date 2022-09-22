a0b4f63ee8eaf74b8d56d033f7f9dd512c650315
Cheng Kai <chengkai@infini.ltd>
2022-09-22 11:45:11 +0800

    handle the last trailing newline characters

--- lib/reader/linenumber/line_number.go
+++ lib/reader/linenumber/line_number.go
@@ -48,14 +48,21 @@ func (r *LineNumberReader) Next() (reader.Message, error) {
 		var offset int64 = 0
 		line := 0
 		var contentLen int64 = 0
+		var content string
+		//获取当前文件换行符的长度。因为在StripNewline这一步的时候，是去掉了每一行的换行符，所以差值为换行符的长度。这里就不再处理一遍换行符了
+		terminatorLength := message.Bytes - len(message.Content)
 		for scanner.Scan() {
-			contentLen = int64(len([]byte(scanner.Text())))
-			offset += contentLen + 1
+			//scanner返回的内容，是已经去掉换行符的
+			content = scanner.Text()
+			contentLen = int64(len([]byte(content)))
+			offset += contentLen + int64(terminatorLength)
 			line++
-			if offset >= (r.currentOffset + int64(message.Bytes)) {
-				message.LineNumbers = append(message.LineNumbers, line)
+			//当前读到的offset，已经超过了用户指定的offset + 当前内容的长度， 那说明offset已经超过范围
+			if offset > (r.currentOffset + contentLen + int64(terminatorLength)) {
+				offset -= contentLen + int64(terminatorLength)
 				break
 			}
+			//当前读到的offset 大于了用户指定的offset，说明已经到了目标为止，记录行号
 			if offset > r.currentOffset {
 				message.LineNumbers = append(message.LineNumbers, line)
 			}
