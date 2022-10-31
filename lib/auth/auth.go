d84ba08ac49a9a0cd7f36c4fc0de27caed03626b
Cheng Kai <chengkai@infini.ltd>
2022-10-31 16:19:29 +0800

    remove unused code

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -7,12 +7,9 @@ package auth
 import (
 	log "github.com/cihub/seelog"
 	"infini.sh/framework/core/agent"
-	"infini.sh/framework/core/config"
-	"sync"
 )
 
 var authenticators []Authenticator
-var once sync.Once
 
 func RegisterAuth(auth Authenticator) {
 	authenticators = append(authenticators, auth)
@@ -34,23 +31,4 @@ func Auth(clusterName, endPoint string, ports ...int) (bool, *agent.BasicAuth) {
 
 type Authenticator interface {
 	Auth(clusterName, endPoint string, ports ...int) (bool, *agent.BasicAuth)
-}
-
-func InitDefaultAuthenticators(cfg *config.Config) bool {
-	if cfg == nil {
-		return false
-	}
-	once.Do(func() {
-		localAuth, err := NewLocalAuthenticator()
-		if err != nil {
-			log.Error(err)
-		}
-		RegisterAuth(localAuth)
-		decryptAuth, err := NewDecryptAuthenticator(cfg)
-		if err != nil {
-			log.Error(err)
-		}
-		RegisterAuth(decryptAuth)
-	})
-	return true
 }
