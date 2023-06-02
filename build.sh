ba4b55c461a930d8c8403a786ea42bc1ab615a11
hardy <luohf@infinilabs.com>
2023-06-02 10:08:05 +0800

    jenkins build shell

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
  #!/bin/bash
 
 #init
+PNAME=agent
 WORKBASE=/home/jenkins/go/src/infini.sh
-WORKDIR=$WORKBASE/agent
+WORKDIR=$WORKBASE/$PNAME
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
 #pull code
@@ -19,52 +20,48 @@ GOROOT="/infini/go-pkgs/go-loongarch" GOPATH="/home/jenkins/go" make build-linux
 #copy-configs
 cd $WORKDIR && cp $WORKBASE/framework/LICENSE bin && cat $WORKBASE/framework/NOTICE NOTICE > bin/NOTICE
 
+cd $WORKDIR/bin
 for t in amd64 386 mips mipsle mips64 mips64le arm5 arm6 arm7 arm64 amd64 loong64 riscv64 ; do
   echo "package-linux-$t"
-  tar cfz ${WORKSPACE}/agent-$VERSION-$BUILD_NUMBER-linux-$t.tar.gz agent-linux-$t agent.yml LICENSE NOTICE 
+  tar cfz ${WORKSPACE}/$PANME-$VERSION-$BUILD_NUMBER-linux-$t.tar.gz $PANME-linux-$t $PANME.yml LICENSE NOTICE 
 done
 
 for t in mac-amd64 mac-arm64 windows-amd64 windows-386 ; do
   echo "package-$t"
-  cd $WORKDIR/bin && zip -r ${WORKSPACE}/agent-$VERSION-$BUILD_NUMBER-$t.zip agent-$t agent.yml LICENSE NOTICE
+  cd $WORKDIR/bin && zip -r ${WORKSPACE}/$PANME-$VERSION-$BUILD_NUMBER-$t.zip $PANME-$t $PANME.yml LICENSE NOTICE
 done
 
-#docker build
-cd $WORKDIR/bin
-cat <<"EOF">Dockerfile
-FROM --platform=$TARGETPLATFORM alpine:3.16.5
+#build image & push
+for t in amd64 arm64 ; do
 
-COPY ["agent-linux-amd64", "agent.yml", "./"]
-CMD ["/agent-linux-amd64"]
+  cat <<EOF>Dockerfile
+MAINTANIER "hardy <luohoufu@gmail.com>"
+FROM --platform=linux/$t alpine:3.16.5
+WORKDIR /opt/$PANME
+
+COPY ["$PANME-linux-$t", "$PANME.yml", "./"]
+
+CMD ["/opt/$PANME/$PANME-linux-$t"]
 EOF
-docker buildx build -t infinilabs/agent-amd64:latest --platform=linux/amd64 -o type=docker .
-cat <<"EOF">Dockerfile
-FROM --platform=$TARGETPLATFORM alpine:3.16.5
 
-COPY ["agent-linux-arm64", "agent.yml", "./"]
-CMD ["/agent-linux-arm64"]
-EOF
-docker buildx build -t infinilabs/agent-arm64:latest --platform=linux/arm64 -o type=docker .
+  docker buildx build -t infinilabs/$PANME-$t:latest --platform=linux/$t -o type=docker .
 
-#推送镜像
-docker push infinilabs/agent-amd64:latest
-docker push infinilabs/agent-arm64:latest
+  docker tag infinilabs/$PANME-$t:latest infinilabs/$PANME-t:$VERSION-$BUILD_NUMBE
+  docker push infinilabs/$PANME-$t:latest
+  docker push infinilabs/$PANME-$t:$VERSION-$BUILD_NUMBE
+done
 
-docker tag infinilabs/agent-amd64:latest infinilabs/agent-amd64:$VERSION-$BUILD_NUMBE
-docker tag infinilabs/agent-amd64:latest infinilabs/agent-arm64:$VERSION-$BUILD_NUMBE
-docker push infinilabs/agent-amd64:$VERSION-$BUILD_NUMBE
-docker push infinilabs/agent-arm64:$VERSION-$BUILD_NUMBE
+#composite tag
+docker buildx imagetools create -t infinilabs/$PANME:latest \
+    infinilabs/$PANME-arm64:latest \
+    infinilabs/$PANME-amd64:latest
 
-docker buildx imagetools create -t infinilabs/agent:latest \
-    infinilabs/agent-arm64:latest \
-    infinilabs/agent-amd64:latest
+docker buildx imagetools create -t infinilabs/$PANME:$VERSION-$BUILD_NUMBE \
+    infinilabs/$PANME-arm64:$VERSION-$BUILD_NUMBE \
+    infinilabs/$PANME-amd64:$VERSION-$BUILD_NUMBE
 
-docker buildx imagetools create -t infinilabs/agent:$VERSION-$BUILD_NUMBE \
-    infinilabs/agent-arm64:$VERSION-$BUILD_NUMBE \
-    infinilabs/agent-amd64:$VERSION-$BUILD_NUMBE
-
-#清理git
+#git reset
 git reset --hard
 
-#清理2周之前的镜像
-docker images |grep agent |grep "weeks ago" |awk '{print $3}' |xargs docker rmi
+#clen weeks ago image
+docker images |grep $PANME |grep "weeks ago" |awk '{print $3}' |xargs docker rmi
