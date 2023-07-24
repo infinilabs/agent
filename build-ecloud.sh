c8ef890c3deb9604468a355d6b396ffe20eee056
hardy <luohf@infinilabs.com>
2023-07-24 10:19:05 +0800

    add ecloud build

--- build-ecloud.sh
+++ build-ecloud.sh
@@ -1,28 +1,20 @@
- #!/bin/bash
+#!/bin/bash
 
 #init
 WORKBASE=/home/jenkins/go/src/infini.sh
 WORKDIR=$WORKBASE/$PNAME
 DEST=/infini/Sync/Release/$PNAME/stable
 
-if [[ $VERSION =~ NIGHTLY ]]; then
-  BUILD_NUMBER=$BUILD_DAY
-  DEST=/infini/Sync/Release/$PNAME/snapshot
-fi
-export DOCKER_CLI_EXPERIMENTAL=enabled
-
-#clean all
-cd $WORKSPACE && git clean -fxd
-
-#change framework branch
+#change branch
 cd $WORKBASE/framework
-git switch -c ecloud-0.3.1 && git pull
+git branch |grep -wq "ecloud-0.3.1" || (git switch -c ecloud-0.3.1 && git pull)
+git branch |grep -wq "ecloud-0.3.1" && (git checkout ecloud-0.3.1 && git pull)
 
-#pull code
-cd $WORKDIR && git clean -fxd
-git stash && git pull origin master
+cd $WORKSPACE
+git branch |grep -wq "ecloud" || (git switch -c ecloud && git pull)
+git branch |grep -wq "ecloud" && (git checkout ecloud && git pull)
 
- #build
+#build
 make clean config build-linux
 
 #copy-configs
@@ -30,10 +22,9 @@ cp -rf $WORKBASE/framework/LICENSE $WORKDIR/bin && cat $WORKBASE/framework/NOTIC
 
 cd $WORKDIR/bin
 for t in amd64 ; do
-  tar zcf ${WORKSPACE}/$PNAME-$VERSION-$BUILD_NUMBER-linux-$t.tar.gz "${PNAME}-linux-$t" $PNAME.yml LICENSE NOTICE 
+  tar zcf ${WORKSPACE}/$PNAME-$VERSION-$BUILD_NUMBER-linux-$t.tar.gz "${PNAME}-linux-$t" $PNAME.yml LICENSE NOTICE
 done
 
 #git reset
-cd $WORKSPACE && git reset --hard
+cd $WORKSPACE && git checkout master && git reset --hard
 cd $WORKBASE/framework && git checkout master && git reset --hard
-
