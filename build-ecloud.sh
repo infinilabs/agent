f1502d8c86227da709ed353c4b9095f70afcb8cb
hardy <luohf@infinilabs.com>
2023-07-25 15:36:17 +0800

    add ecloud build

--- build-ecloud.sh
+++ build-ecloud.sh
@@ -6,17 +6,23 @@ WORKDIR=$WORKBASE/$PNAME
 
 #change branch
 cd $WORKBASE/framework
-git branch |grep -wq "ecloud-0.3.1" || (git checkut -b ecloud-0.3.1 && git pull origin/ecloud-0.3.1 ecloud-0.3.1)
-git branch |grep -wq "ecloud-0.3.1" && (git checkout ecloud-0.3.1 && git pull origin/ecloud-0.3.1 ecloud-0.3.1)
+if [ "$(git symbolic-ref --short HEAD)"=="master" ]; then
+  git checkout ecloud-0.3.1 
+fi
+
+cd $WORKBASE/framework-vendor
+if [ "$(git symbolic-ref --short HEAD)"=="master" ]; then
+  git checkout ecloud-0.3.1
+fi
 
 cd $WORKDIR
-git branch |grep -wq "ecloud" || (git checkout -b ecloud && git pull origin/ecloud ecloud)
 if [ "$(git symbolic-ref --short HEAD)"=="master" ]; then
-  git branch |grep -wq "ecloud" && (git checkout ecloud && git pull origin/ecloud ecloud)
+  git checkout ecloud && git pull
 fi
 
 #build
 make clean config build-linux
+make config build-darwin
 
 #copy-configs
 cp -rf $WORKBASE/framework/LICENSE $WORKDIR/bin && cat $WORKBASE/framework/NOTICE $WORKDIR/NOTICE > $WORKDIR/bin/NOTICE
@@ -26,6 +32,11 @@ for t in amd64 ; do
   tar zcf ${WORKSPACE}/$PNAME-$VERSION-$BUILD_NUMBER-linux-$t.tar.gz "${PNAME}-linux-$t" $PNAME.yml LICENSE NOTICE
 done
 
+for t in mac-amd64; do
+  [ -f ${WORKSPACE}/$PNAME-$VERSION-$BUILD_NUMBER-$t.zip ] && cp -rf ${WORKSPACE}/$PNAME-$VERSION-$BUILD_NUMBER-$t.zip $DEST
+done
+
 #git reset
 cd $WORKDIR && git checkout master && git reset --hard
 cd $WORKBASE/framework && git checkout master && git reset --hard
+cd $WORKBASE/framework-vendor && git checkout master && git reset --hard
