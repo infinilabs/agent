b0d29857eaab50ce6baedf8c7a91fd9d26d20f83
medcl <m@medcl.net>
2023-10-20 16:10:19 +0800

    add api to discovery unknown nodes

--- plugin/api/init.go
+++ plugin/api/init.go
@@ -14,10 +14,10 @@ func InitAPI() {
 	agentAPI := AgentAPI{}
 
 	//discovery local nodes
-	api.HandleAPIMethod(api.GET, "/elasticsearch/nodes/_discovery", agentAPI.getESNodes)
+	api.HandleAPIMethod(api.GET, "/elasticsearch/node/_discovery", agentAPI.getESNodes)
+	api.HandleAPIMethod(api.POST, "/elasticsearch/node/_info", agentAPI.getESNodeInfo) //get node info by connect to this node
 
 	api.HandleAPIMethod(api.POST, "/elasticsearch/_register", agentAPI.registerESNode)
 	api.HandleAPIMethod(api.POST, "/elasticsearch/logs/_list", agentAPI.getElasticLogFiles)
 	api.HandleAPIMethod(api.POST, "/elasticsearch/logs/_read", agentAPI.readElasticLogFile)
 }
-
