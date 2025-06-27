6ecade98f5301cbfdda30124061748fc04cd621d
Medcl <m@medcl.net>
2025-06-27 11:57:54 +0800

    chore: add go module (#40)
    * chore: add go module
    
    * chore: simplify main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module infini.sh/agent
+
+go 1.23.3
--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 	terminalFooter := ""
 
 	app := framework.NewApp("agent", "A light-weight but powerful cloud agent.",
-		util.TrimSpaces(config.Version), util.TrimSpaces(config.BuildNumber), util.TrimSpaces(config.LastCommitLog), util.TrimSpaces(config.BuildDate), util.TrimSpaces(config.EOLDate), terminalHeader, terminalFooter)
+		config.Version, config.BuildNumber, config.LastCommitLog, config.BuildDate, config.EOLDate, terminalHeader, terminalFooter)
 
 	app.Init(nil)
 
