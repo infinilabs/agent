13e755cefdc3a6ca3a69a4f0a84969c00d0bd674
Steve Lau <stevelauc@outlook.com>
2026-04-08 10:57:06 +0800

    feat: enable CORS support for discovery & logs APIs

--- plugin/api/init.go
+++ plugin/api/init.go
@@ -4,7 +4,10 @@
 
 package api
 
-import "infini.sh/framework/core/api"
+import (
+	"infini.sh/framework/core/api"
+	"infini.sh/framework/modules/security/http_filters"
+)
 
 type AgentAPI struct {
 	api.Handler
@@ -14,8 +17,12 @@ func InitAPI() {
 	agentAPI := AgentAPI{}
 
 	// Discovery & logs — require login
-	api.HandleUIMethod(api.GET, "/elasticsearch/node/_discovery", agentAPI.getESNodes, api.RequireLogin())
-	api.HandleUIMethod(api.POST, "/elasticsearch/node/_info", agentAPI.getESNodeInfo, api.RequireLogin())
-	api.HandleUIMethod(api.POST, "/elasticsearch/logs/_list", agentAPI.getElasticLogFiles, api.RequireLogin())
-	api.HandleUIMethod(api.POST, "/elasticsearch/logs/_read", agentAPI.readElasticLogFile, api.RequireLogin())
+	api.HandleUIMethod(api.GET, "/elasticsearch/node/_discovery", agentAPI.getESNodes, api.RequireLogin(), api.Feature(http_filters.FeatureCORS))
+	api.HandleUIMethod(api.OPTIONS, "/elasticsearch/node/_discovery", agentAPI.getESNodes, api.RequireLogin(), api.Feature(http_filters.FeatureCORS))
+	api.HandleUIMethod(api.POST, "/elasticsearch/node/_info", agentAPI.getESNodeInfo, api.RequireLogin(), api.Feature(http_filters.FeatureCORS))
+	api.HandleUIMethod(api.OPTIONS, "/elasticsearch/node/_info", agentAPI.getESNodeInfo, api.RequireLogin(), api.Feature(http_filters.FeatureCORS))
+	api.HandleUIMethod(api.POST, "/elasticsearch/logs/_list", agentAPI.getElasticLogFiles, api.RequireLogin(), api.Feature(http_filters.FeatureCORS))
+	api.HandleUIMethod(api.OPTIONS, "/elasticsearch/logs/_list", agentAPI.getElasticLogFiles, api.RequireLogin(), api.Feature(http_filters.FeatureCORS))
+	api.HandleUIMethod(api.POST, "/elasticsearch/logs/_read", agentAPI.readElasticLogFile, api.RequireLogin(), api.Feature(http_filters.FeatureCORS))
+	api.HandleUIMethod(api.OPTIONS, "/elasticsearch/logs/_read", agentAPI.readElasticLogFile, api.RequireLogin(), api.Feature(http_filters.FeatureCORS))
 }
